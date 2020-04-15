b4980904423100f9127b3a109bb3a1576ffa1ac6
Grant Imbo <grant.imbo@gmail.com>
2020-04-15 13:59:33 +0800

    update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 // variables
 let menuDrawer = document.querySelector('.main-mobile-menu'),
     menu = document.querySelector('.mobile .menu'),
-    siteLink = window.origin,
     menuItem = document.querySelector('a.haschild');
 
 // functions
@@ -25,7 +24,7 @@ function getContents() {
     document.addEventListener('click', function(e) {
 		if ( e.target.matches('.links a') ) {
             e.preventDefault()
-            let link = siteLink + e.target.pathname;
+            let link = e.target.pathname;
 
             [].forEach.call(document.querySelectorAll('.links a'), function(e) { 
                 e.classList.remove('active')
